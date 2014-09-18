56847d5fe114752056421abcabfb8b32819e1157
Rob Vermaas <rob.vermaas@gmail.com>
2014-09-18 11:06:03 +0200

    Add simple travis-ci check for tarball.

---
+++ maintainers/scripts/travis/basic-checks.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+# Install Nix
+bash <(curl https://nixos.org/nix/install)
+source $HOME/.nix-profile/etc/profile.d/nix.sh
+
+echo "Building tarball"
+nix-build -A tarball
+
+
