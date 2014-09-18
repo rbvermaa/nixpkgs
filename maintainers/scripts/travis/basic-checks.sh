0279acc9f3d672a2555767ae62089cff30e8fb4e
Rob Vermaas <rob.vermaas@gmail.com>
2014-09-18 12:02:14 +0200

    Build release-small in stead of just tarball.

--- maintainers/scripts/travis/basic-checks.sh
+++ maintainers/scripts/travis/basic-checks.sh
@@ -5,6 +5,6 @@ bash <(curl https://nixos.org/nix/install)
 source $HOME/.nix-profile/etc/profile.d/nix.sh
 
 echo "Building tarball"
-nix-build -j 2 -A tarball $(dirname $(readlink -f $0))/../../../pkgs/top-level/release.nix
+nix-build -j 2 $(dirname $(readlink -f $0))/../../../pkgs/top-level/release-small.nix
 
 
